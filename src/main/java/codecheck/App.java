577616727ec60372a2b16066099fe231ba298778
OyamaErika <oyama.erika@tis.co.jp>
2018-06-14 00:31:21 +0900

    作成

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,29 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+		String comand = args[0];
+		String str = args[1];
+		StringBuffer ans = new StringBuffer();
+
+		if ("encode".equals(comand)) {
+			str = Integer.toString(Integer.parseInt(str),9);
+			String[] splitStr = str.split("");
+
+			for (int i = 0, l = splitStr.length; i < l; i++) {
+				int num = Integer.parseInt(splitStr[i]);
+				int a = (int)'A';
+				String s =String.valueOf((char)(a + num));
+				ans = ans.append(s);
+			}
 		}
+
+		if ("decode".equals(comand)) {
+
+		}
+		System.out.println(ans);
+
+
 	}
+
+
 }
